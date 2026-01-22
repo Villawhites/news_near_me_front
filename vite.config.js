fba38e046f64c9ae7111477202d8d0d9ad6706e6
Daniel <daniel@aureolab.cl>
2026-01-22 16:50:42 -0300

    cambios para render.com

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        allowedHosts: ['news-near-me-front.onrender.com'],
         host: '0.0.0.0',
         port: 3000,
         proxy: {
